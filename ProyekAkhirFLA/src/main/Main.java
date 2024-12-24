3ea3f93604ff3b57883d17e7dcdc17a90a29236c
Ricardo Eric Wijaya <ricardo.wijaya@binus.edu>
2024-12-24 16:35:01 +0700

    Tambah menu

--- ProyekAkhirFLA/src/main/Main.java
+++ ProyekAkhirFLA/src/main/Main.java
@@ -1,32 +1,71 @@
 package main;
 
+import java.util.Scanner;
+
 import model.BookingService;
 import model.Room;
 import model.User;
 import singleton.Database;
 
 public class Main {
-
+	Database database = Database.getDatabaseInstances();
+	Scanner sc = new Scanner(System.in);
+	public void MenuAfterRegister(User user) {
+		while(true) {
+			System.out.println("Hotel Booking System");
+			System.out.println("1. Book Available Room");
+			System.out.println("2. See History Booking");
+			System.out.println("3. Back to register page");
+			System.out.print(">> ");
+			int choice = sc.nextInt();sc.nextLine();
+			switch (choice) {
+			case 1:
+				database.showAvailableRooms();
+				BookingService bookingSytem = new BookingService();
+				System.out.println("Enter your number hotel that you want to book: ");
+				int option = sc.nextInt()-1;sc.nextLine();
+				Room roomToBook = database.getAvailableRooms().get(option);
+				bookingSytem.bookRoom(user, roomToBook);
+				break;
+			case 2:
+				System.out.println("List Booking: ");
+				user.showBookings();
+				break;
+			case 3:
+				return;
+			}
+		}
+		
+	}
 	public Main() {
 		// TODO Auto-generated constructor stub
-		Database database = Database.getDatabaseInstances();
-        User user1 = new User("A");
-        User user2 = new User("B");
-
-        database.showAvailableRooms();
-
-        user1.register();
-        user2.register();
-
-        BookingService bookingSystem = new BookingService();
-
-        Room roomToBook = database.getAvailableRooms().get(0); 
-        bookingSystem.bookRoom(user1, roomToBook);
-
-        user1.showBookings();
-        user2.showBookings();
-
-        database.showAvailableRooms();
+        
+        while(true) {
+        	System.out.println("Hotel Booking System");
+        	System.out.println("1. Register");
+        	System.out.println("2. Exit");
+        	System.out.print(">> ");
+        	int choice = sc.nextInt();sc.nextLine();
+        	switch (choice) {
+			case 1:
+				System.out.println("Welcome to Register");
+				System.out.print("Enter your name to register: ");
+				String name = sc.nextLine();
+				System.out.print("Are you sure to register with this "+name+" Y/N ?: ");
+				String confirmation = sc.nextLine();
+				if(confirmation.equals("Y")) {
+					User newUser = new User(name);
+					newUser.register();
+					MenuAfterRegister(newUser);
+				}else {
+					System.out.println("Back to register page");
+				}
+				break;
+			case 2:
+				System.out.println("Thank you for using our booking system");
+				return;
+			}
+        }
 	}
 
 	public static void main(String[] args) {
